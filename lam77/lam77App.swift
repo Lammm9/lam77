31067c432f154e8e62daa070532809b18f4645bd
Lammm9 <nquelam1998@gmail.com>
2024-06-22 09:36:30 +0700

    Initial Commit

---
+++ lam77/lam77App.swift
@@ -0,0 +1,17 @@
+//
+//  lam77App.swift
+//  lam77
+//
+//  Created by lam on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct lam77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
